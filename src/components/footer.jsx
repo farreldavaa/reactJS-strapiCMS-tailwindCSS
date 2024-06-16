a938988ee467a30469c532ae629d571dafb3f9c4
farreldavaa <farrel.dava@binus.ac.id>
2024-06-17 00:24:29 +0700

    adding footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import {FaInstagram, FaGithub} from 'react-icons/fa';
+
 
 const Footer = () => {
     return(
-        <div>
-            Footer
+        <div className="w-full bg-[#02044A] text-gray-300 py-8 px-2"> 
+            <div className="max-w-[1240px] mx-auto grid grid-cols-3 md:grid-cols-6 border-b-2 border-gray-600 py-5">
+                <div>
+                    <h6 className="font-bold uppercase py-2">
+                        Community Guidelines
+                    </h6>
+                    <ol>
+                       <li className="py-1">Instagram</li> 
+                       <li className="py-1">Facebooks</li> 
+                       <li className="py-1">Youtube</li> 
+                    </ol>
+                </div>
+            </div>
+                <div className="flex flex-col max-w-[1240px] px-2 py-4 m-auto justify-between sm:flex-row text-center text-gray-500">
+                        <p>
+                            2024 Ordnyr, All right reserved.
+                        </p>
+                        <div className="flex justify-between gap-10 sm:w-[300px] pt-4 text-2x1">
+                            <FaInstagram/>
+                            <FaGithub/>
+                        </div>
+                </div>
         </div>
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     let handleClick = ()=>setToggle(!toggle)
 
     return (
-        <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-md relative'>
+        <div className='w-full h-[80px] z-10 bg-white fixed drop-shadow-md relative'>
             <div className='flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
                 <div className='flex items-center'>
                     <img src="" alt="" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]' />
